073d1f44419c3520ec587f1f0e70393ded3e0157
Tair Yunussov <tair.yunussov@ps.kz>
2019-08-13 16:24:41 +0600

    removed unnecessary quotes

--- roles/mail_postfixadmin/templates/create_postfix_admin.sh
+++ roles/mail_postfixadmin/templates/create_postfix_admin.sh
@@ -4,13 +4,13 @@
     sleep 0.5;
     echo "admin@{{ vhost }}";
     sleep 0.5;
-    echo '"{{ lookup('password', '/root/062-pfxadmin_admin_password chars=ascii_letters,digits length=32') }}"';
+    echo "{{ lookup('password', '/root/062-pfxadmin_admin_password chars=ascii_letters,digits length=32') }}";
     sleep 0.5;
-    echo '"{{ lookup('password', '/root/062-pfxadmin_admin_password') }}"';
+    echo "{{ lookup('password', '/root/062-pfxadmin_admin_password') }}";
     sleep 0.5;
     echo "y";
     sleep 0.5;
-    echo '"{{ vhost }}"';
+    echo "{{ vhost }}";
     sleep 0.5;
     echo "y";
 } | bash /var/www/postfixadmin/scripts/postfixadmin-cli admin add
